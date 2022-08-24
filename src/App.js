1c634d80970552f7313fbdcc9a7fb0043892fbb3
newnewK <nyuheekim@Nyuheeui-MacBookPro.local>
2022-08-24 12:44:47 +0900

    prototype shop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./componets/Footer";
+import Header from "./componets/Header";
+import Orders from "./componets/Orders";
+import Prototypes from "./componets/Prototypes";
+import AppStateProvider from "./providers/AppStateProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStateProvider>
+      <Header />
+      <div className="container">
+        <Prototypes />
+        <Orders />
+        <Footer />
+      </div>
+    </AppStateProvider>
   );
 }
 
